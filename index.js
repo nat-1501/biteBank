02c3873b9381d2398fe8787b6bc9965c09365f8b
Nat-1501 <natalisoares90@gmail.com>
2022-05-13 09:28:44 -0300

    Comportamentos de classes

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 class Cliente {
     nome;
     cpf;
+   
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+        
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-
-
 cliente1.nome = "Marcos";
 cliente1.cpf = 11223344556;
-cliente1.agencia = 1001;
-cliente1.saldo = 0; 
 
+const cliente2 = new Cliente();
 cliente2.nome = "Maria";
 cliente2.cpf = 33323344556;
-cliente2.agencia = 1001;
-cliente2.saldo = 0; 
 
+const cliente3 = new Cliente();
 cliente3.nome = "Julio";
 cliente3.cpf = 22223344556;
-cliente3.agencia = 1001;
-cliente3.saldo = 30; 
 
-console.log(cliente1, cliente2);
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.saldo = 0;
+ContaCorrenteRicardo.agencia = 1001;
+
+console.log(ContaCorrenteRicardo.saldo);
+ContaCorrenteRicardo.saldo = 100;
+console.log(ContaCorrenteRicardo.saldo);
+ContaCorrenteRicardo.sacar(250);
+
+
+console.log(ContaCorrenteRicardo.saldo);
+
+
+
+
+console.log(cliente1);
+console.log(cliente2)
