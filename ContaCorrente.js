8c31d7677c68b17e0fa1a31fe74d3d9213b30ead
Nat-1501 <natalisoares90@gmail.com>
2022-05-14 14:47:46 -0300

    atriutos estaticos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,45 +1,54 @@
 import { Cliente } from "./Cliente.js";
 
-export class ContaCorrente {
+export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-   _cliente;
+    _cliente;
+   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
    _saldo = 0;
+    
 
-   set cliente(novoValor){
-       if(novoValor instanceof Cliente){
-       this.cliente = novoValor;
-       }
-   }
 
-   get cliente(){
-       return this._cliente;
-   }
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
 
-   get saldo(){
-       return this._saldo;
-   }
+    get cliente(){
+        return this._cliente;
+    }
 
-   constructor(cliente, agencia) {
-       this.agencia = agencia;
-       this.cliente = cliente;
-   }
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
 
     sacar(valor){
-        if(this._saldo >= valor) {
+        if(this._saldo >= valor){
             this._saldo -= valor;
             return valor;
         }
-        
     }
 
-    depositar(valor) {
-        if(valor <= 0) {
-            return;  
-        }
-        this._saldo += valor;
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
     }
-    transferir(valor, conta) {
+
+    tranferir(valor, conta){
+        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        
     }
 }
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente("Ricardo", 11223344556);
-const cliente2 = new Cliente("Maria", 33323344556);
-const cliente3 = new Cliente("Julio", 22223344556);
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
 
-const ContaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-ContaCorrenteRicardo.depositar(500);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
 const conta2 = new ContaCorrente(102, cliente2);
 
-
-ContaCorrenteRicardo.transferir(valor, conta2);
-
 let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
+
+console.log(contaCorrenteRicardo);
 
