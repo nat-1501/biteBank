628432ecf8d74fe57e8182b3b7b4ba28b943afd2
Nat-1501 <natalisoares90@gmail.com>
2022-05-14 14:18:04 -0300

    construtores

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11223344556;
+const cliente1 = new Cliente("Ricardo", 11223344556);
+const cliente2 = new Cliente("Maria", 33323344556);
+const cliente3 = new Cliente("Julio", 22223344556);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Maria";
-cliente2.cpf = 33323344556;
 
-const cliente3 = new Cliente();
-cliente3.nome = "Julio";
-cliente3.cpf = 22223344556;
-
-const ContaCorrenteRicardo = new ContaCorrente();
-ContaCorrenteRicardo.saldo = 0;
-ContaCorrenteRicardo.agencia = 1001;
-ContaCorrenteRicardo.cliente = cliente1;
+const ContaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 ContaCorrenteRicardo.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 85425632514;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
 
-ContaCorrenteRicardo.transferir(200, conta2);
 
-console.log(conta2);
-console.log(ContaCorrenteRicardo);
+ContaCorrenteRicardo.transferir(valor, conta2);
+
+let valor = 200;
 
